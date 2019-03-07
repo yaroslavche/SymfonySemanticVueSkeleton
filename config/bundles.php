36ddb608dfc6cfa8be4dadca008afc020bdf5eaa
yaroslavche <yaroslav429@gmail.com>
2019-03-07 18:01:54 +0200

    Install encore and symfony/web-server-bundle --dev

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
